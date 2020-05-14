b0db3c8b13001373720629677e1e5d289e0a9f75
Joe Groocock <me@frebib.net>
2020-05-14 19:48:17 +0100

    cmd/get: download multiple mods at once
    Launch a go func for each download thread, and wait for each to
    complete. Consolidate the list of files and download them all together
    at the end after identifying everything to download.
    
    Signed-off-by: Joe Groocock <me@frebib.net>

--- cmd/get.go
+++ cmd/get.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"context"
-	"errors"
 	"sync"
 
 	"github.com/frebib/mcmod/api"
@@ -46,76 +45,117 @@ func cmdDoGet(c *cli.Context) (err error) {
 		return
 	}
 
-	mod, err := api.ClientFromContext(ctx).Lookup(ctx, c.Args().First())
-	if err != nil {
-		return err
-	}
-	ctx, log = modlog.SetContextLogger(ctx, log.WithField("mod", mod.Slug))
-	log.WithField("id", mod.ID).Info("found mod")
+	var filelist []*api.File
+	var filelock sync.Mutex
 
-	filter := &ModFilter{Release: reqRelease, Version: reqVer}
-	files, err := listFilterMods(ctx, mod.ID, filter)
-	if err != nil {
-		return err
-	}
+	var wait sync.WaitGroup
+	var waitCh = make(chan struct{}, 1)
+	var errs = make(chan error, 1)
 
-	// Pick the latest release
-	modFile := files[0]
-	log.WithField("file-id", modFile.ID).
-		Tracef("chose '%s' as latest file", modFile.FileName)
-	var toDownload = []*api.File{&modFile}
+	count := c.Args().Len()
+	wait.Add(count)
 
-	// Download optional dependencies, if any, unless otherwise specified
-	getDeps := !c.Bool(flagNoDeps.Name)
-	if len(modFile.Dependencies) > 0 && getDeps {
-		log.Debug("no dependencies found")
-	} else if getDeps {
-		log.Debugf("resolving %d dependencies", len(modFile.Dependencies))
-		mu := new(sync.Mutex)
-		wg := new(sync.WaitGroup)
+	ctx, cancel := context.WithCancel(ctx)
+	for _, mod := range c.Args().Slice() {
+		go func(ctx context.Context, name string) {
+			err := func(ctx context.Context, name string) error {
+				defer wait.Done()
 
-		for _, dep := range modFile.Dependencies {
-			wg.Add(1)
-			go func(ctx context.Context, depID int) {
-				defer wg.Done()
-				// Update logger to display correct log details for the dep
-				ctx, _ = modlog.SetContextLogger(ctx,
-					log.WithFields(logrus.Fields{
-						"mod":    depID,
-						"dep-of": mod.Slug,
-					}),
-				)
-				depMod, err := api.ClientFromContext(ctx).AddonByID(ctx, depID)
+				mod, err := api.ClientFromContext(ctx).Lookup(ctx, name)
 				if err != nil {
-					log.WithError(err).Warnf("failed to lookup dependency")
-				} else if depMod != nil {
-					// Add the name now that we know what it is
-					ctx, _ = modlog.SetContextLogger(ctx,
-						log.WithField("mod", depMod.Slug),
-					)
+					return err
+				}
+				ctx, log = modlog.SetContextLogger(ctx, log.WithField("mod", mod.Slug))
+				log.WithField("id", mod.ID).Info("found mod")
+
+				filter := &ModFilter{Release: reqRelease, Version: reqVer}
+				files, err := listFilterMods(ctx, mod.ID, filter)
+				if err != nil {
+					return err
 				}
 
-				depFiles, err := listFilterMods(ctx, depID, filter)
-				if err != nil {
-					var unused *ErrNoMatch
-					if errors.As(err, &unused) {
-						log.Warnf("no download found, skipping")
-					} else {
-						return
+				// Pick the latest release
+				modFile := files[0]
+				log.WithField("file-id", modFile.ID).
+					Tracef("chose '%s' as latest file", modFile.FileName)
+
+				filelock.Lock()
+				filelist = append(filelist, &modFile)
+				filelock.Unlock()
+
+				// Download optional dependencies, unless otherwise specified
+				if !c.Bool(flagNoDeps.Name) {
+					log.Debugf("resolving %d dependencies", len(modFile.Dependencies))
+					var depwait sync.WaitGroup
+
+					for _, dep := range modFile.Dependencies {
+						depwait.Add(1)
+						go func(ctx context.Context, depID int) {
+							defer depwait.Done()
+							// Update logger to display correct log details for the dep
+							ctx, _ = modlog.SetContextLogger(ctx,
+								log.WithFields(logrus.Fields{
+									"mod":    depID,
+									"dep-of": mod.Slug,
+								}),
+							)
+							depMod, err := api.ClientFromContext(ctx).AddonByID(ctx, depID)
+							if err != nil {
+								log.WithError(err).Warnf("failed to lookup dependency")
+							} else if depMod != nil {
+								// Add the name now that we know what it is
+								ctx, log = modlog.SetContextLogger(ctx,
+									log.WithField("mod", depMod.Slug),
+								)
+							}
+
+							depFiles, err := listFilterMods(ctx, depID, filter)
+							if err != nil {
+								return
+							}
+							if len(depFiles) > 0 {
+								filelock.Lock()
+								filelist = append(filelist, &depFiles[0])
+								filelock.Unlock()
+							} else {
+								log.Warnf("no download found, skipping")
+							}
+						}(ctx, dep.AddonID)
 					}
+					depwait.Wait()
+					log.Debugf("found an additional %d files", len(files)-1)
 				}
-				if len(depFiles) > 0 {
-					mu.Lock()
-					toDownload = append(toDownload, &depFiles[0])
-					mu.Unlock()
-				}
-			}(ctx, dep.AddonID)
-		}
-		wg.Wait()
-		log.Debugf("found an additional %d files", len(toDownload)-1)
+				return nil
+			}(ctx, name)
+			if err != nil {
+				errs <- err
+			}
+		}(ctx, mod)
 	}
 
-	for _, dl := range toDownload {
+	log.Debug("wait for threads to end")
+
+	go func() {
+		wait.Wait()
+		log.Debug("all threads finished")
+		close(waitCh)
+	}()
+
+	select {
+	case <-waitCh:
+		break
+	case err = <-errs:
+		if err != nil {
+			cancel()
+			// Wait for all cancelled threads to exit
+			wait.Wait()
+			return err
+		}
+	}
+
+	log.Debugf("identified %d mods, proceeding to download", count)
+
+	for _, dl := range filelist {
 		// Calculate final path+filename for mod output
 		outFile := c.String(flagOutputFile.Name)
 		outDir := c.String(flagDirectory.Name)
